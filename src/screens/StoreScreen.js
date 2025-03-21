e79035168b2d16eae09d662f07f194d7606e8439
kerosene2562 <ipzk241_roa@student.ztu.edu.ua>
2025-03-21 14:56:53 +0200

    added game cards

--- src/screens/StoreScreen.js
+++ src/screens/StoreScreen.js
@@ -1,8 +1,7 @@
 import React, { useState, useRef } from 'react';
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
+import { FlatList, View, Text, Button, TextInput, ImageBackground } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import styled from 'styled-components';
-
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const StyledMainView = styled.View`
@@ -11,7 +10,7 @@ const StyledMainView = styled.View`
 `;
 
 const StyledHeader = styled.View`
-    background-color: rgb(184, 184, 184);
+    background-color: rgb(204, 204, 204);
     height: 100px;
 `;
 
@@ -31,11 +30,11 @@ const StyledMainText = styled.Text`
 `;
 
 const IconWrapper = styled.View`
-  width: 40px;  
-  height: 40px;
-  border-radius: 30px; 
-  background-color:  rgb(8, 1, 71); 
-  overflow: hidden;
+    width: 40px;  
+    height: 40px;
+    border-radius: 30px; 
+    background-color:  rgb(8, 1, 71); 
+    overflow: hidden;
 `;
 
 const StyledInput = styled.TextInput`
@@ -45,7 +44,71 @@ const StyledInput = styled.TextInput`
     border: ${(props) => (props.focused ? '1px' : 'none')};
     padding-left: 10px;
     height: 40px;
-    transition: width 0.3s ease;
+`;
+
+const Card = styled.View`
+    flex: 1;
+    background-color: white;
+    width: 330px;
+    height: 270px;
+    border-radius: 20px;
+    elevation: 5;
+    margin-left: 20px;
+    overflow: hidden;
+`;
+
+const CardBackgroungImg = styled.ImageBackground`
+    flex: 1;
+    padding-left: 15px;
+    padding-top: 50%;
+`;
+
+const CardText = styled.Text`
+    font-size: 30px;
+    color: white;
+    font-weight: 700;
+`;
+
+const CardTextSmall = styled.Text`
+    font-size: 15px;
+    font-weight: 500;
+    color: rgb(214, 214, 214);
+`;
+
+const MoreInfoBlock = styled.View`
+    width: 100%;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding-right: 10px;
+`;
+
+const PriceBlock = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+`;
+
+const SystemsBlock = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+`;
+
+const StrikethroughText = styled.Text`
+    text-decoration-line: line-through;
+    margin-right: 10px;
+    font-size: 16px;
+    color: rgb(173, 173, 173);
+`;
+
+const DiscountText = styled.Text`
+    font-size: 16px;
+    backgroundColor: green;
+    borderRadius: 10px;
+    margin-right: 5px;
+    padding: 5px;
+    color: white;
 `;
 
 export function Store(){
@@ -64,6 +127,45 @@ export function Store(){
         setShowSearchIcon(true);
     }
 
+    const cardData = [
+        { id: '1', title: 'Dead by Daylight', recomended: 'Recomended by your friend, Player', price: '18', discount: '50', systems: ['windows', 'mac', 'linux'], src: require('../assets/deadByDaylight.jpg')},
+        { id: '2', title: 'Borderlands 3', recomended: 'Recomended by your friend, Player', price: '23', discount: '30', systems: ['windows'], src: require('../assets/borderlands3.jpg') },
+        { id: '3', title: 'Doom 2016', recomended: 'Recomended by your friend, Player', price: '30', discount: '20', systems: ['windows'], src: require('../assets/doom2016.jpg') },
+        { id: '4', title: 'This war of mine', recomended: 'Recomended by your friend, Player', price: '10', discount: '0', systems: ['windows', 'linux'], src: require('../assets/twom.jpg') },
+        { id: '5', title: 'Harts of iron 4', recomended: 'Recomended by your friend, Player', price: '40', discount: '90', systems: ['windows', 'mac'], src: require('../assets/hoi4.jpg') },
+      ];
+    
+    const renderItem = ({ item }) => {
+        const discountedPrice = item.price * (1 - item.discount / 100);
+        return(
+            <Card>
+                <CardBackgroungImg source ={item.src}>
+                    <CardText>{item.title}</CardText>
+                    <CardTextSmall>{item.recomended}</CardTextSmall>
+                    <MoreInfoBlock>
+                        <PriceBlock>
+                            { item.discount > 0 ? (
+                                <>
+                                    <DiscountText>-{item.discount}%</DiscountText>
+                                    <StrikethroughText>{item.price}$</StrikethroughText>
+                                    <CardTextSmall>{discountedPrice.toFixed(2)}$</CardTextSmall>
+                                </>
+                            ) : (
+                                <CardTextSmall>{item.price}$</CardTextSmall>
+                            )
+                            }
+                        </PriceBlock>
+                        <SystemsBlock>
+                            { item.systems.includes('windows') && <Ionicons name="logo-windows" size={25} color="white" />}
+                            { item.systems.includes('mac') && <Ionicons name="logo-apple" size={25} color="white" />}
+                            { item.systems.includes('linux') && <Ionicons name="logo-tux" size={25} color="white" />}
+                        </SystemsBlock>
+                    </MoreInfoBlock>
+                </CardBackgroungImg>
+            </Card> 
+        );
+    }
+    
     return(
         <StyledMainView>
             <StyledHeader>
@@ -76,6 +178,15 @@ export function Store(){
                     { showSearchIcon && <Ionicons name="search-outline" size={40} color={'black'} onPress={handleFocus} /> }
                 </StyledViewBlock>
             </StyledHeader>
+
+            <FlatList 
+                data = {cardData}
+                renderItem = {renderItem}
+                keyExtractor={(item) => item.id}
+                horizontal
+                showsHorizontalScrollIndicator = {false}
+            />
+
             <StatusBar style="auto" />
         </StyledMainView>
     );
